21f49a153455f22e16ce915c0d2fe519b2d6df0f
AllenDev1 <81756508+AllenDev1@users.noreply.github.com>
2024-09-03 12:54:00 +0545

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,62 +1,61 @@
-# Snake game in Python
-# Use arrow keys to play
-
 import curses
 from random import randint
 
-# Initialize curses
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
+def main(stdscr):
+    # Initialize curses
+    curses.curs_set(0)  # Hide cursor
+    curses.noecho()     # Don't show key presses
+    stdscr.nodelay(1)   # Non-blocking input
+    stdscr.timeout(100) # Refresh every 100ms
+    
+    # Create a window
+    win = curses.newwin(20, 60, 0, 0)
+    win.keypad(1)
+    win.border(0)
 
-# Initialize snake
-snake = [(4, 10), (4, 9), (4, 8)]
-food = (10, 20)
-win.addch(food[0], food[1], '*')
+    # Initialize snake and food
+    snake = [(4, 10), (4, 9), (4, 8)]
+    food = (10, 20)
+    win.addch(food[0], food[1], '*')
 
-# Game loop
-while True:
-    # Move snake
-    key = win.getch()
-    if key == curses.KEY_UP:
-        new_head = (snake[0][0] - 1, snake[0][1])
-    elif key == curses.KEY_DOWN:
-        new_head = (snake[0][0] + 1, snake[0][1])
-    elif key == curses.KEY_LEFT:
-        new_head = (snake[0][0], snake[0][1] - 1)
-    elif key == curses.KEY_RIGHT:
-        new_head = (snake[0][0], snake[0][1] + 1)
-    else:
-        new_head = (snake[0][0], snake[0][1])
+    key = curses.KEY_RIGHT  # Initial direction
 
-    snake.insert(0, new_head)
+    while True:
+        new_key = win.getch()
+        key = key if new_key == -1 else new_key
 
-    # Check if snake hit border
-    if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59:
-        break
+        # Determine new head position based on key input
+        head = snake[0]
+        if key == curses.KEY_UP:
+            new_head = (head[0] - 1, head[1])
+        elif key == curses.KEY_DOWN:
+            new_head = (head[0] + 1, head[1])
+        elif key == curses.KEY_LEFT:
+            new_head = (head[0], head[1] - 1)
+        elif key == curses.KEY_RIGHT:
+            new_head = (head[0], head[1] + 1)
+        else:
+            new_head = head
 
-    # Check if snake hit itself
-    if snake[0] in snake[1:]:
-        break
+        # Check for collision with border or self
+        if (new_head[0] in [0, 19] or new_head[1] in [0, 59]) or new_head in snake:
+            break
 
-    # Check if snake ate food
-    if snake[0] == food:
-        food = (randint(1, 18), randint(1, 58))
-        win.addch(food[0], food[1], '*')
-    else:
-        tail = snake.pop()
-        win.addch(tail[0], tail[1], ' ')
+        snake.insert(0, new_head)
 
-    # Update snake on screen
-    win.addch(snake[0][0], snake[0][1], 'o')
+        # Check if snake has eaten the food
+        if new_head == food:
+            food = (randint(1, 18), randint(1, 58))
+            win.addch(food[0], food[1], '*')
+        else:
+            tail = snake.pop()
+            win.addch(tail[0], tail[1], ' ')
 
-# End game
-curses.nocbreak()
-win.keypad(0)
-curses.echo()
-curses.endwin()
-print("Score:", len(snake))
+        win.addch(snake[0][0], snake[0][1], 'o')
+
+    # End game
+    stdscr.addstr(10, 25, "Game Over! Press any key to exit.")
+    stdscr.refresh()
+    stdscr.getch()
+
+curses.wrapper(main)
